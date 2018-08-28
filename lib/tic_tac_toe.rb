3beb9666b430bd02fe9bd496de07a1832d4f0724
Jacqueline Chenault <adigitalnative@gmail.com>
2018-08-28 15:47:27 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -54,7 +54,7 @@ def turn(board)
   input = gets.strip
   index = input_to_index(input)
   if valid_move?(board, index)
-    move(board, index, current_player)
+    move(board, index, current_player(board))
     turn(board)
   else
     puts "Invalid selection, try again"
