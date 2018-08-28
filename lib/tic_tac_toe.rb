26f3418f08cf441eb6c33faaaac8b4277194ef74
Jacqueline Chenault <adigitalnative@gmail.com>
2018-08-28 15:32:59 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -54,4 +54,16 @@ def valid_move?(board, index)
   end
 end
 
-def turn
+def turn(board)
+  puts "Welcome to Tic Tac Toe!"
+  display_board(board)
+  puts "Please enter 1-9:"
+  input = gets.strip
+  index = input_to_index(input)
+  if valid_move?(board, index)
+    move(board, index)
+    display_board(board)
+  else
+    turn(board)
+  end
+end
