58b699163951bdcd8e737f4cc6c9b5a3a620810b
Jacqueline Chenault <adigitalnative@gmail.com>
2018-08-28 15:48:10 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -49,18 +49,31 @@ end
 
 
 def turn(board)
-  display_board(board)
   puts "Please enter 1-9:"
-  input = gets.strip
-  index = input_to_index(input)
+  user_input = gets.strip
+  index = input_to_index(user_input)
   if valid_move?(board, index)
     move(board, index, current_player(board))
-    turn(board)
+    display_board(board)
   else
-    puts "Invalid selection, try again"
     turn(board)
   end
 end
+
+
+# def turn(board)
+#   display_board(board)
+#   puts "Please enter 1-9:"
+#   input = gets.strip
+#   index = input_to_index(input)
+#   if valid_move?(board, index)
+#     move(board, index, current_player(board))
+#     turn(board)
+#   else
+#     puts "Invalid selection, try again"
+#     turn(board)
+#   end
+# end
 
 def turn_count(board)
   counter = 0
